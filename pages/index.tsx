e0fc810778ec6b519bb1e3dbe4f7445a99a73e54
rainwildest <rainwildest@163.com>
2022-11-03 17:40:06 +0800

    添加机器学习 demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import gql from "graphql-tag";
 import { useQuery } from "@apollo/client";
 import io from "socket.io-client";
 import { useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
 
 const FeedQuery = gql`
   query FeedQuery {
@@ -59,6 +60,58 @@ const Blog = () => {
         clineOnline();
       }
     });
+
+    // import * as tfvis from "@tensorflow/tfjs-vis";
+  }, []);
+
+  const test = () => {
+    const heights = [150, 160, 170];
+    const weights = [40, 50, 60];
+
+    const tfvis = require("@tensorflow/tfjs-vis");
+    tfvis.render.scatterplot(
+      { name: "身高体重训练数据" },
+      { values: heights.map((x, i) => ({ x, y: weights[i] })) },
+      { xAxisDomain: [140, 200], yAxisDomain: [30, 90] }
+    );
+
+    // 归一化
+    const inputs = tf.tensor(heights).sub(150).div(20);
+    const labels = tf.tensor(weights).sub(40).div(20);
+
+    const model = tf.sequential(); // 建立模型
+    model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+    model.compile({
+      loss: tf.losses.meanSquaredError, // 均方误差
+      optimizer: tf.train.sgd(0.1) //学习率
+    });
+
+    model
+      .fit(inputs, labels, {
+        batchSize: 3,
+        epochs: 200,
+        callbacks: tfvis.show.fitCallbacks({ name: "训练过程" }, ["loss"])
+      })
+      .then(() => {
+        const output = model.predict(tf.tensor([180]).sub(150).div(20));
+        // console.log(output.mul(20).add(40).dataSync()[0]);
+
+        const val = (output as tf.Tensor<tf.Rank>)
+          .mul(20)
+          .add(40)
+          .dataSync()[0];
+
+        console.log(`如果身高为 180，那么预测体重为 ${val}`);
+      });
+
+    // inputs.print();
+    // labels.print();
+  };
+
+  useEffect(() => {
+    if (window) {
+      test();
+    }
   }, []);
 
   if (loading) {
